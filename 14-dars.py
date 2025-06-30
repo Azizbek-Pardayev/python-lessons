2dc3b80e5447f00e75d3c50f3a193c65b4da9b51
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-06-30 11:57:23 +0500

    Lug'atlar bilan ishlash. Python 14-dars.
    python.sariq.dev kursi 14-dars kodlari

---
+++ 14-dars.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 30 10:10:22 2025
+
+@author: Azizbek
+"""
+
+#14-dars Lug'at bilan tanishuv
+
+#car_0={'avto':'Tracker','rang':'qora'} #Lug'atda katta qavs qo'yiladi
+#print(car_0['avto'])
+#print(car_0['rang'])
+
+#mevalar={'olma':'12000','tarvuz':'100000','qovun':'9000'}
+#print(f"Olma narxi {mevalar['olma']} so'm")
+
+#talaba_0={'ism':'Vali','yosh':'25','t_yil':'2000'}
+#print(f"Talabaning ismi {talaba_0['ism']}, {talaba_0['t_yil']}-yil tug'ilgan, \
+# Hozir {talaba_0['yosh']} yoshda")
+
+# Yangi qiymat qo'shish
+#talaba_0['kurs'] = 3
+#talaba_0['fakultet'] = 'informatika'
+#talaba_0['ism']='abdulloh'
+#print(talaba_0)
+
+#talaba_1={}
+#talaba_1['ism']='qobil rasulov'
+#talaba_1['kurs']=2
+#talaba_1['yosh']=20
+#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
+
+#talaba_0={'ism':'Vali','yosh':'25','t_yil':'2000'}
+#del talaba_0['ism']
+#print(talaba_0)
+
+#eng_uz={'apple':'olma','apricot':"o'rik",'banana':'banan',}
+#meva=eng_uz.get('cherry', 'bunday meva mavjud emas')
+#print(meva)
+
+#ukam={'ism':'ali valiyev','yosh':'17','sinfi':'11'}
+#print(f"Ukam {ukam['ism'].title()} yoshi {ukam['yosh']} da {ukam['sinfi']} o'quvchisi")
+
+#s_taom={'Dadam':'osh','Onam':'shashlik','Ukam':'jiz'}
+#print(s_taom['Dadam'])
+#print(s_taom['Onam'])
+#print(s_taom['Ukam'])
+
+#python={'integer':'butun son','float':"o'nlik son",'string':"so'z",
+#        'print':'konsolga kodni chiqarish','del':"o'chirish",'upper':'katta harf'}
+#print(python)
+
+#eng_uz={'apple':'olma','apricot':"o'rik",'banana':'banan',}
+#a=input("So'z kiriting: ").lower() #lower qo'yiladigan joy
+#if a in eng_uz:
+#    print(f"{eng_uz[a]}")
+#else:
+#    print("Bunday so'z mavjud emas")
+
