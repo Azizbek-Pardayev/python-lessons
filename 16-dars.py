f86eed61ff233ae51dc2ecfad99959e5ea8678f5
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-07-08 00:15:14 +0500

    Nesting
    yangi

---
+++ 16-dars.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  5 18:25:22 2025
+
+@author: Azizbek
+"""
+
+#car0={'avto':'nexia 3',
+#      'rang':'oq',
+#      'yil':'2019',
+#      'km':'6000',
+#      'narx':'9000$'}
+#car1={'avto':'damas',
+#      'rang':'oq',
+#      'yil':'2016',
+#      'km':'6000',
+#      'narx':'6000$'}
+#car2={'avto':'tracker 2',
+#      'rang':'qora',
+#      'yil':'2025',
+#      'km':'2000',
+#      'narx':'18000$'}
+#cars=[car0,car1,car2]
+#for car in cars:
+#    print(f"{car['avto'].title()}",
+#          f"{car['rang']} rang",
+#          f"{car['yil']}-yil",
+#          f"{car['km']}-km",
+#          f"{car['narx']}")
+
+#malibus=[]
+#for n in range(10):
+#    new_car={'avto':'malibu',
+#          'rang':'oq',
+#          'yil':None,
+#          'km':'0',
+#          'narx':None,
+#          'karobka':'avto'}
+#    malibus.append(new_car)
+#for malibu in malibus:
+#    print(malibu)
+#for malibu in malibus[:3]:
+#    malibu['rang']='qizil'
+#for malibu in malibus[3:]:
+#    malibu['rang']='qora'
+#for malibu in malibus[6:]:
+#    malibu['karobka']='mexanika' 
+#for malibu in malibus:
+#    if malibu['karobka']=='mexanika':
+#        malibu['narx']=35000
+#    else:
+#        malibu['narx']=40000
+#for malibu in malibus:
+#    print(malibu)   
+
+# LUG'AT ICHIDA RO'YXAT
+
+#dasturchilar={'ali':['c++','python'],
+#              'vali':['js', 'c++'],
+#              'hasan':['php','sql'],
+#              'husan':['html','c++']}
+#for ism, tillar in dasturchilar.items():
+#    print(f"\n{ism.title()} quyidagi dasturlash tillarini biladi:", end='')
+#    for til in tillar:
+#        print(f"{til.upper()}",end=' ')
+
+#hamkasblar = {
+#    'ali':{'familiya':'valiyev',
+#           'tyil':1995,
+#           'malumot':'oliy',
+#           'tillar':['python','c++']
+#           },
+#    'vali':{'familiya':'aliyev',
+#            'tyil':2001,
+#            'malumot':"o'rta-maxsus",
+#            'tillar':['html', 'css', 'js']},
+#    'hasan':{'familiya':'husanov',
+#             'tyil':1999,
+#             'malumot':'maxsus',
+#             'tillar':['python','php']}
+#    }
+#for ism, info in hamkasblar.items():
+#    print(f"\n{ism.title()} {info['familiya'].title()}, "
+#          f"{info['tyil']}-yilda tug'ilgan. "
+#          f"Ma'lumoti: {info['malumot']}. \n"
+#          "Quyidagi dasturlash tillarini biladi:")
+#    for til in info['tillar']:
+#        print(til.upper())
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
