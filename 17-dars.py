03459e2c91448cfb9e98172ae0cb1c04fa719bfe
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-07-17 20:28:39 +0500

    While
    uzr

---
+++ 17-dars.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 17 19:18:43 2025
+
+@author: Azizbek
+"""
+
+#17-dars WHILE
+
+#INPUT
+#ism=input("Salom ismingiz nima? ")
+#savol=f"Salom {ism.title()}, yoshingiz nechchida? "
+#yosh=input(savol)
+#yosh=int(yosh)
+#height=input("Bo'yingiz necha metr? ")
+#height=float(height)
+
+#WHILE
+#son=1 #Songa 1 qiymat beramiz
+#while son<=5: # toki son 5 dan kichik yoki teng ekan kod bajariladi
+#    print(son, end=' ')
+#    son=son+1    # 1 dan 5 gacha sanaydigan dastur
+    #son+=1 # songa 1 qo'shish uchun 
+
+#WHILE AND INPUT
+#print("Kiritilgan sonning kvadratini qaytaruvchi dastur")
+#savol="Istalgan son kiriting"
+#savol+="(dasturni to'xtatish uchun exit deb yozing): "
+#qiymat=""
+#while qiymat !='exit':
+#    qiymat=input(savol)
+#    if qiymat !='exit':
+#        print(float(qiymat)**2)
+#print("Daastur tugadi")
+
+#ISHORA
+#print("Kiritilgan sonning kvadratini qaytaruvchi dastur")
+#savol="Istalgan son kiriting"
+#savol+="(dasturni to'xtatish uchun exit deb yozing): "
+#ishora=True
+#while ishora:
+#    qiymat=input(savol)
+#    if qiymat == 'exit':
+#        ishora=False
+#    else:
+#        print(float(qiymat)**2)
+#print("Dastur tugadi.")
+
+#BREAK WHILE
+#print("Kiritilgan sonning kvadratini qaytaruvchi dastur")
+#savol="Istalgan son kiriting"
+#savol+="(dasturni to'xtatish uchun exit deb yozing): "
+#while True:
+#    qiymat=input(savol)
+#    if qiymat=='exit':
+#        break
+#    else:
+#        print(float(qiymat)**2)
+#print("Dastur tugadi!")
+
+#BREAK FOR
+#sonlar=list(range(1,11))
+#for son in sonlar:
+#    if son==5:
+#        break
+#    else:
+#        print(f"{son} ning kvadrati {son**2} ga teng")
+
+
+#Continue
+#sonlar=list(range(1,11))
+#for son in sonlar:
+#    if son==5:
+#        continue
+#    else:
+#        print(f"{son} ning kvadrati {son**2} ga teng")
+
+#CONTINUE WHILE
+#son=0
+#while son<10:
+#    son+=1
+#    if son%2!=0:
+#        continue
+#   else:
+#        print(son)
+    
+
+
+
+
+
+
+
+
+
+
+
