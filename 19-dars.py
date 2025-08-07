04c5151626a47c921bf102d1adc43c9b03ad0c82
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-08-07 12:33:32 +0500

    19-dars
    funksiya

---
+++ 19-dars.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  7 11:36:13 2025
+
+@author: Azizbek
+"""
+
+#19-dars Funksiyalar
+
+#def salom_ber():
+#    """Foydalanuvchiga salom beruvchi funksiya""" #funksiya infosi
+#    print("Assalomu alaykum!")
+
+#def salom_ber(ism):
+#    """Foydalanuvchi ismini qabul qilib salom beruvchi funksiya"""#DOCSTRING
+#    print(f"Assalomu alaykum, hurmatli {ism.title()}")
+#
+#salom_ber("olim")
+
+#def toliq_ism(ism, familiya):
+#    """Ism familiyasini bilib oladigan funksiya"""
+#    print(f"Foydalanuvchi ismi: {ism.title()},\n",
+#          f"Foydalanuvchi familiyasi: {familiya.title()}")
+#    
+#toliq_ism("Javohir", "Anvarov")
+
+#def yosh_hisobla(tugilgan_yil, hozirgi_yil=2025):
+#    """Yosh hisoblaydigan funksiya"""
+#    print(f"Jasmina {hozirgi_yil-tugilgan_yil} yoshda")
+
+#yosh_hisobla(2019)
+
+#def son_kvadrati(son):
+#    """Sonning kvadratini hisoblaydigan funksiya"""
+#    print(f"Sonning kvadrati {son**2} ga teng")
+#son_kvadrati(25)
+
+#def toq_juft(son):
+#    """Toq juftligini aniqlaydigan funksiya"""
+#    if son%2:
+ #       print(f"{son} toq son")
+#    else:
+#        print(f"{son} juft son")
+#toq_juft(259)
+    
+#def sonlar(son_1, son_2):
+#    """Sonlarning kattasini konsolga chiqaruvchi funksiya"""
+#    if son_1>son_2:
+#        print(f"{son_1} katta {son_2} dan")
+  #  elif son_1==son_2:
+ #       print("Ekovi teng")
+#    else:
+#        print(f"{son_2} katta {son_1} dan")
+#sonlar(21,21)
+
+#def bolinish(son):
+#    for n in range(2, 11):
+#        if not son%n:
+#            print(f"{son} {n} ga qoldiqsiz bo'linadi.")
+#bolinish(875)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
