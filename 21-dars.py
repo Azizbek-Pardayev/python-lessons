aff9d81be6b0a3b2ba5f658671bcaf2889d6f190
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-08-12 11:00:33 +0500

    21-dars
    fdg

---
+++ 21-dars.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 11 10:32:25 2025
+
+@author: Azizbek
+"""
+
+#def bahola(ismlar):
+#    baholar={}
+#    while ismlar:
+#        ism=ismlar.pop()
+#        baho=input(f"Talaba {ism.title()} bahosini kiriting: ")
+#        baholar[ism]=int(baho)
+#    return baholar
+#talabalar=['ali','vali','hasan','husan']
+#baholar=bahola(talabalar[:])#Nusxa olish kerak ro'yxat buzilmasligi uchun
+#print(baholar)
+#print(talabalar)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
