209edd60f462e9f6d7394e334e1ac6ea8760b6c3
Azizbek-Pardayev <azizbekpardayev01@gmail.com>
2025-08-12 11:01:02 +0500

    22-dars
    a

---
+++ 22-dars.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 12 10:31:06 2025
+
+@author: Azizbek
+"""
+
+#22-dars Moslashuvchan funksiya *args va **kwargs
+
+#def summa(*sonlar):
+#    """Sonlarning yig'indisini hisoblovchi funksiya"""
+#    return sum(sonlar)
+#yigindi=summa(1,2,3,4,5,6,7,8,9)
+#def summa(*sonlar):
+#    yigindi=0
+#    for son in sonlar:
+#        yigindi += son
+#    return yigindi
+#print(summa(1,2,3,4))
+
+#def avto_info(kompaniya,model,**malumotlar):
+#    """Avto haqidagi ma'lumotlarni lug'at ko'rinishida chiqaruvchi funksiya"""
+#    malumotlar['kompaniya']=kompaniya
+#    malumotlar['model']=model
+#    return malumotlar
+
+#avto1 = avto_info('gm', 'Tracker', rangi='qora', karobka='avtomat')
+
+#def kopaytma(*sonlar):
+#    bir=1
+#    for son in sonlar:
+#        bir*=son
+#    return bir
+
+#print(kopaytma(2,3,4,5))
+
+#def talaba(ism,familiya, **malumotlar):
+#    malumotlar['ism']=ism
+#    malumotlar['familiya']=familiya
+#    return malumotlar
+#
+#talaba1 = talaba('olim', 'hakimov', yosh=20, bahosi=4, malumoti="o'rta")
+#print(talaba1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
